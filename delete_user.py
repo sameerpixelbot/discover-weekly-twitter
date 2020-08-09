d1f5b8e0031b2834dbc5c8b774830ef6bbb64468
sameer <sameerpixelbot@gmail.com>
2020-08-09 18:38:36 +0530

    doing this commit before changes in data base

---
+++ delete_user.py
@@ -0,0 +1,32 @@
+#this file is to manually delete users
+import pymongo
+
+#getting the screen_name
+screen_name=input('Please enter the name of user : ')
+
+#connecting to db
+client=pymongo.MongoClient()
+db=client.twitter
+
+#getting users
+users=[]
+for user in db.users.find({}):
+    users.append(user)
+
+
+for user in users:
+    
+    if user['user']['screen_name']==screen_name:
+        _id=user['_id']
+    
+    for i in range(len(user['edges'])):
+        
+        if user['edges'][i]['screen_name']==screen_name:
+            pos=i
+    
+    user['edges'].pop(pos)
+    db.users.update_one({'_id':user['_id']},
+                        {'$set':{'edges':user['edges']}})
+
+#removing user
+db.users.remove_one({'_id':_id})
